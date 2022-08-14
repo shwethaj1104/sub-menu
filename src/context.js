237be4882316097819234efdab942cf46a6d4f43
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-14 10:27:55 +0530

    styling

--- src/context.js
+++ src/context.js
@@ -4,37 +4,37 @@ import sublinks from './data'
 
 const AppContext = React.createContext();
 
-export const AppProvider = ({children})=>{
+export const AppProvider = ({ children }) => {
 
-const [isSidebarOpen,setIsSidebarOpen] = useState(false);
-const [isSubmenuOpen,setIsSubmenuOpen] = useState(false);
-const [location,setLocation] = useState({})
-const [page,setPage] = useState({page:'',links:[]})
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [location, setLocation] = useState({})
+    const [page, setPage] = useState({ page: '', links: [] })
 
-const openSidebar =()=>{
-    setIsSidebarOpen(true)
-}
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
 
-const closeSidebar =()=>{
-    setIsSidebarOpen(false)
-}
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
 
-const openSubmenu =(text,coordinates)=>{
-    const page = sublinks.find((link)=> link.page === text)
-    setPage(page)
-    setLocation(coordinates)
-    setIsSubmenuOpen(true)
-}
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text)
+        setPage(page)
+        setLocation(coordinates)
+        setIsSubmenuOpen(true)
+    }
 
-const closeSubmenu =()=>{
-    setIsSubmenuOpen(false)
-}
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false)
+    }
 
     return <AppContext.Provider
-     value={{closeSubmenu,closeSidebar,openSubmenu,openSidebar,isSubmenuOpen,location,isSidebarOpen,page}}>{children}
-     </AppContext.Provider>
+        value={{ closeSubmenu, closeSidebar, openSubmenu, openSidebar, isSubmenuOpen, location, isSidebarOpen, page }}>{children}
+    </AppContext.Provider>
 }
 
-export const useGlobalContext = ()=>{
+export const useGlobalContext = () => {
     return useContext(AppContext)
 }
